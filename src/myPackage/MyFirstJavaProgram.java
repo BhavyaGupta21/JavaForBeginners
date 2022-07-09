6167835d0389f9574e41c911c529a34b2bf84149
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-07-09 17:42:39 +0530

    First Java Program

---
+++ src/myPackage/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) { // Java program starts with main method
+        System.out.println("This is my first Java program"); // This is a print statement
+    }
+}
