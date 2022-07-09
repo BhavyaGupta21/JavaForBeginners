12a8e85bf0f9c0ca0ebcf3798e1ecb9e485741bd
BhavyaGupta21 <72260665+BhavyaGupta21@users.noreply.github.com>
2022-07-09 18:12:18 +0530

    DataTypes

---
+++ src/dataTypes/DataTypes.java
@@ -0,0 +1,26 @@
+package dataTypes;
+
+// There are basically 2 types of data types
+// Primitive & Derived
+// Primitive contains int, double, float, char, boolean, byte, short etc...
+// Derived contains Array, ArrayList, HashMap, String etc...
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double d = 20.5;
+        char c = 'H';
+        boolean b = true; // Boolean accepts only 2 values, i. e, true or false
+
+        String s = "Welcome"; // String is inbuilt class in java, also used as data type
+        // String is collection of characters, should be included in double quotes
+
+        System.out.println(a); // Prints value of a
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
--- src/myPackage/MyFirstJavaProgram.java
+++ src/myPackage/MyFirstJavaProgram.java
@@ -3,5 +3,10 @@ package myPackage;
 public class MyFirstJavaProgram {
     public static void main(String[] args) { // Java program starts with main method
         System.out.println("This is my first Java program"); // This is a print statement
+
+        int a; // Declaration
+        a = 10; // Initialization
+
+        int b = 20; // Combined
     }
 }
